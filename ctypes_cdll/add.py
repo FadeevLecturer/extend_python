2a5cc6fc7691814f4d42d2f008d85bba0d3dffcd
Fadeev Egor <fedeev.ep@physics.msu.ru>
2022-01-11 16:50:17 +0300

    Вызов dll методами модуля ctypes.

---
+++ ctypes_cdll/add.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import ctypes
+import argparse
+
+
+def parse_args():
+    default_path = os.path.join("..", "libshared", "Debug", "libshared.dll")
+    
+    parser = argparse.ArgumentParser(description='Adds two integers.')
+    parser.add_argument('terms',  type=int, nargs='+', help='Two terms to sum')
+    parser.add_argument('--dll', default=default_path)
+    
+    args = parser.parse_args()
+    assert len(args.terms) == 2, "takes exactly two parameters" 
+    return args
+
+
+
+args = parse_args()
+path_to_dll = args.dll
+a, b = args.terms
+
+dll = ctypes.cdll.LoadLibrary(path_to_dll)
+dll.add.argtypes = (ctypes.c_int, ctypes.c_int)
+dll.add.restype = ctypes.c_int
+
+
+
+c1 = dll.add(a, b)
+c2 = a + b
+print(f"C++:\t{c1}")
+print(f"python:\t{c2}")
---
+++ ctypes_cdll/sum.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import ctypes
+import argparse
+
+import numpy as np
+np.set_printoptions(precision=2)
+
+def parse_args():
+    default_path = os.path.join("..", "libshared", "Debug", "libshared.dll")
+    
+    parser = argparse.ArgumentParser(description='Sums a random numpy array.')
+    parser.add_argument('shape',  type=int, nargs='+', help='Two terms to sum')
+    parser.add_argument('--dll', default=default_path)
+    
+    args = parser.parse_args()
+    return args
+
+
+args = parse_args()
+path_to_dll = args.dll
+shape = args.shape
+
+dll = ctypes.cdll.LoadLibrary(path_to_dll)
+double_pointer = ctypes.POINTER(ctypes.c_double)
+dll.sum.argtypes = (double_pointer, ctypes.c_size_t)
+dll.sum.restype = ctypes.c_double
+
+array = np.random.randn(*shape)
+array = array.astype(ctypes.c_double)
+print(array)
+pointer = array.ctypes.data_as(double_pointer)
+nelem = array.size
+
+s1 = dll.sum(pointer, array.size)
+s2 = array.sum()
+print(f"C++:\t{s1}")
+print(f"numpy:\t{s2}")
